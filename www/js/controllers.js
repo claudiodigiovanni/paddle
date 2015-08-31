df001c296d7690b8b23c2aecd1f141c4733291ba
Claudio Di Giovanni <claudio.digiovanni@gmail.com>
2015-08-31 09:28:34 +0200

    primo

---
+++ www/js/controllers.js
@@ -0,0 +1,141 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+
+  var TestObject = Parse.Object.extend("TestObject");
+  var testObject = new TestObject();
+  testObject.save({foo: "bar"}).then(function(object) {
+    console.log("yay! it worked");
+  });
+
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+
+.controller('SetAvalability', function($scope, $stateParams, Utility) {
+
+  var days = ['LUN','MAR','MER','GIO','VEN','SAB','DOM'];
+
+  var firstDateOfMonth = Utility.getDayOfFirstDateOfMonth(3,2015);
+  var pos = days.indexOf(firstDateOfMonth);
+
+  var datex = new Date();
+  //var daysInMonth = Utility.getDaysInMonth(datex.getMonth(),datex.getYear());
+
+  var daysInMonth = Utility.getDaysInMonth(3,2015);
+
+  console.log('daysInMonth:' + daysInMonth);
+
+  var myDay = 1;
+
+
+  var week1 = [];
+  for (var i = 0; i < pos; i++){
+    week1.push('-');
+  }
+  for (var j = pos ; j < 7 ;j++){
+    week1.push(myDay);
+    myDay++;
+  }
+  var week2 = [];
+  for (var t = 0 ; t < 7 ;t++){
+    week2.push(myDay);
+    myDay++;
+  }
+  var week3 = [];
+  for (var y = 0 ; y < 7 ;y++){
+    week3.push(myDay);
+    myDay++;
+  }
+  var week4 = [];
+  for (var x = 0 ; x < 7 ;x++){
+    week4.push(myDay);
+    myDay++;
+  }
+
+console.log('prima di week5:' + myDay);
+  var week5 = [];
+  if (myDay <= daysInMonth){
+    for (var z = 0 ; z < 7 ;z++){
+      if (myDay <= daysInMonth)
+        week5.push(myDay);
+      else {
+        week5.push('-');
+      }
+      myDay++;
+    }
+  }
+console.log('prima di week6:' + myDay);
+  var week6 = [];
+  if (myDay <= daysInMonth){
+    for (var w = 0 ; w < 7 ;w++){
+      if (myDay <= daysInMonth)
+        week6.push(myDay);
+      else {
+        week6.push('-');
+      }
+      myDay++;
+    }
+  }
+
+
+
+  console.log(week1);
+  console.log(week2);
+  console.log(week3);
+  console.log(week4);
+  console.log(week5);
+  console.log(week6);
+
+
+
+
+
+
+  $scope.days = days;
+  var booked = [];
+  var selected = [];
+
+  var date = new Date() ;
+  var currentDate = date.getDate();
+
+  console.log('ccc'+ currentDate);
+  //date.setDate(date.getDate() + 8);
+  //console.log (days[(date.getDay() + 6 ) % 7]);
+
+  var getDayStatus = function(day){
+
+    if (day < currentDate){
+      return 'Disabled';
+    }
+
+
+  };
+
+
+
+
+});
---
+++ www/js/services.js
@@ -0,0 +1,74 @@
+angular.module('starter.services', [])
+
+.factory('Utility',function(){
+
+  return {
+    /*
+    The month passed in is 1 for January, 2 for February and so on through to 12 for December.
+
+The way the above code works is that the month passed to the Date constructor is actually 0 based (i.e. 0 is January, 1 is February etc) so it is in effect creating a date for the day 0 of the next month. Because day 0 equates to the last day of the previous month the number returned is effectively the number of days for the month we want.
+    */
+    getDaysInMonth: function(month,year) {
+      return new Date(year, month, 0).getDate();
+    },
+    getDayOfFirstDateOfMonth: function(month,year){
+
+      var days = ['LUN','MAR','MER','GIO','VEN','SAB','DOM'];
+      var date = new Date (year, month-1, 1);
+      return days[(date.getDay() + 6 ) % 7];
+      //return date.getDay();
+    },
+    test: function() {
+      console.log('test');
+    }
+  };
+})
+
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+});
