0c17f46ea6e3fd74a51755443d3b88d383b93329
Claudio Di Giovanni <claudio.digiovanni@gmail.com>
2015-09-02 23:44:21 +0200

    ok

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -29,22 +29,69 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
+/*
+{user1,data,[1,2,3,6,8], campo1, maestro1}
+1) Seleziona dispo maestro
+2) Seleziona Prenotazoni
+
+*/
+.controller('BookCoach', function($scope, $stateParams, config,MockData) {
+
+  var prenotazioni = MockData.getPrenotazioni (8,2015);
+  var disponibilitaCoach = MockData.getDisponibilitaCoach (8,2015);
+
+  var avalabilities = []
+  _.each(disponibilitaCoach,function (d){
+    _.each(d.ranges, function(r){
+        var px = _.where(prenotazioni,{date:d.date,ranges:[r]});
+        if (px && px.length < config.TennisCourtsNumber)
+          avalabilities.push({date:d.date,range:r});
+    })
+  })
+  console.log(avalabilities);
+})
+
+
+.controller('BookCourt', function($scope, $stateParams, config,MockData,Utility) {
+
+  var prenotazioni = MockData.getPrenotazioni (8,2015);
+  var daysInMonth = Utility.getDaysInMonth(8,2015)
+  //In una giornata ci sono 48 slot prenotabili....
+  var days = _.range(1,parseInt(daysInMonth));
+  var avalabilities = []
+
+  _.each(days,function(day){
+      var px = _.where(prenotazioni,{date:new Date('2015/08/' + day)});
+      if (px && px.length < config.TennisCourtsNumber)
+        avalabilities.push({date:new Date('2015/08/' + day)});
+  })
+  console.log(avalabilities);
+})
+
+
+
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
   };
 })
 
-.controller('SetAvalability', function($scope, $stateParams, Utility) {
+.controller('SetAvalability', function($scope, $stateParams, Utility, MockData) {
 
   var currentDate = new Date();
-  $scope.currentMonth = currentDate.getMonth();
+  $scope.currentMonth = parseInt(currentDate.getMonth());
+  $scope.currentYear = currentDate.getFullYear();
 
   $scope.changeMonth = function (pos){
 
       $scope.currentMonth = parseInt($scope.currentMonth) + parseInt(pos);
 
   }
+
+  $scope.$watch('currentMonth', function() {
+    //alert("month:" + $scope.currentMonth);
+    $scope.weeks = Utility.getCalendar($scope.currentMonth,currentDate.getFullYear());
+  })
 
 
   $scope.updateFn = function(){
@@ -53,42 +100,87 @@ angular.module('starter.controllers', [])
 
   $scope.getRangeStatus = function(pos){
     //alert('getRangeStatus' + pos);
-    if (pos==1)
+    if (selectedRanges.indexOf(pos) != -1){
       return "positive";
-    else if (pos==2)
-      return "energized";
+    }
+      return "light";
   }
 
+  $scope.setRangeStatus = function(pos){
+    //alert('getRangeStatus' + pos);
+    if (selectedRanges.indexOf(pos) != -1){
+      selectedRanges.splice(selectedRanges.indexOf(pos),1);
+    }
+    else {
+      selectedRanges.push(pos);
+    }
+
+  }
 
   var weekDays = ['LUN','MAR','MER','GIO','VEN','SAB','DOM'];
-  $scope.weeks = Utility.getCalendar($scope.currentMonth,currentDate.getYear());
+  $scope.weeks = Utility.getCalendar($scope.currentMonth,currentDate.getFullYear());
 
   $scope.weekDays = weekDays;
-  var booked = [3,4,6,11,21];
+  var booked = MockData.getBookings(8,2015);
+
   $scope.booked = booked;
-  var selected = [];
-  $scope.selected = selected;
+  var selectedDays = [];
+  var selectedRanges = [];
+  $scope.selectedDays = selectedDays;
+
+  $scope.bookedDay = false;
+
+  $scope.deleteBooking = function(day){
+      booked.splice(booked.indexOf($scope.selectedBookedDay) ,1);
+      $scope.bookedDay = false;
+      selectedRanges = [];
+
+  }
 
   $scope.dayClicked = function(day){
     if (day == "-")
       return;
-    if (booked.indexOf(day) != -1){
-      booked.splice(booked.indexOf(day),1);
+    var e = _.find(booked,function(obj){
+        return (obj.date.getDate() == day);
+    });
+    if ( e ){
+      //booked.splice(e,1);
+      selectedRanges = e.ranges;
+      $scope.selectedBookedDay = e;
+      $scope.bookedDay = true;
+      selectedDays = [];
+
+
     }
-    else if (selected.indexOf(day) != -1){
-        selected.splice(selected.indexOf(day),1);
+    else if (selectedDays.indexOf(day) != -1){
+        selectedDays.splice(selectedDays.indexOf(day),1);
+        $scope.bookedDay = false;
       }
     else{
-        selected.push(day);
+        selectedDays.push(day);
+        $scope.bookedDay = false;
     }
   }
 
   $scope.getDayStatus = function(day){
-    if (booked.indexOf(day) != -1){
+
+
+    if ($scope.bookedDay){
+      var mydate = new Date($scope.selectedBookedDay.date);
+      if (day == mydate.getDate()){
+        return "bookedSelected";
+      }
+
+    }
+
+    var e = _.find(booked,function(obj){
+        return (obj.date.getDate() == day);
+    });
+    if ( e ){
       return 'booked';
     }
-    else if (selected.indexOf(day) != -1){
-      return 'selected';
+    else if (selectedDays.indexOf(day) != -1){
+      return 'selectedDays';
     }
     else {
       return 'avalaible'
@@ -96,6 +188,17 @@ angular.module('starter.controllers', [])
   };
 
   $scope.addRange = function(range){
+
+    _.each(selectedDays,function(value, key, list){
+
+      var d = $scope.currentYear + "/" + $scope.currentMonth + "/" + value;
+      booked.push({date: new Date(d), ranges:selectedRanges})
+
+
+    })
+
+    selectedDays = [];
+    selectedRanges = [];
 
   }
 
