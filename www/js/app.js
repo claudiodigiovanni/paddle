ce21602423b1999bf6393a3c6341486be2a66991
Claudio Di Giovanni <claudio.digiovanni@gmail.com>
2017-01-24 21:54:51 +0100

    ip server

--- www/js/app.js
+++ www/js/app.js
@@ -114,10 +114,10 @@ angular.module('starter', ['ionic','ionic.service.core','ionic.service.push','ng
 
   slotsNumber: 48,
   playersLevels:6,
-  //serverAddress: 'http://192.168.1.71:3000/',
-  //webServerAddress: 'http://192.168.1.71:8080/'
-  serverAddress: 'http://localhost:3000/',
-  webServerAddress: 'http://localhost:8080/'
+  serverAddress: 'http://178.62.243.221:3000/',
+  webServerAddress: 'http://178.62.243.221:8081/'
+  //serverAddress: 'http://localhost:3000/',
+  //webServerAddress: 'http://localhost:8080/'
 
 })
 
